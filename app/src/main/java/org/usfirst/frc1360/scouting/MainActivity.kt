ab794f6edbf6170bd4df67e8e857e284810a79ca
Jason Heddle <jason.heddle@hsc.on.ca>
2016-11-13 11:21:39 -0500

    Form completed

--- app/src/main/java/org/usfirst/frc1360/scouting/MainActivity.kt
+++ app/src/main/java/org/usfirst/frc1360/scouting/MainActivity.kt
@@ -14,10 +14,24 @@ import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 internal class MainActivity : Activity() {
     override fun onCreate(savedInstancesState: Bundle?) {
         super.onCreate(savedInstancesState)
         setContentView(R.layout.activity_main)
+        highGoalNum.setText("0")
+        lowGoalMissNum.setText("0")
+        chevalDeFrisseCrossNum.setText("0")
+        portcullisCrossNum.setText("0")
+        lowGoalNum.setText("0")
+        highGoalMissNum.setText("0")
+        moatCrossNum.setText("0")
+        rampartsCrossNum.setText("0")
+        drawbridgeCrossNum.setText("0")
+        sallyportCrossNum.setText("0")
+        rockWallCrossNum.setText("0")
+        roughTerrainCrossNum.setText("0")
+        lowBarCrossNum.setText("0")
     }
 }
