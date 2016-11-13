24d78d31263f66195c44e6cfe03d71f3f5a57c38
Jason Heddle <jason.heddle@hsc.on.ca>
2016-11-13 14:26:49 -0500

    Buttons actually do somethingn now

--- app/src/main/java/org/usfirst/frc1360/scouting/MainActivity.kt
+++ app/src/main/java/org/usfirst/frc1360/scouting/MainActivity.kt
@@ -14,6 +14,8 @@ import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.LinearLayout
+import android.widget.ScrollView
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 internal class MainActivity : Activity() {
@@ -33,5 +35,135 @@ internal class MainActivity : Activity() {
         rockWallCrossNum.setText("0")
         roughTerrainCrossNum.setText("0")
         lowBarCrossNum.setText("0")
+
+        lowBarCrossNumMinus.setOnClickListener {
+            lowBarCrossNum.setText((lowBarCrossNum.text.toString().toInt() - 1).toString())
+            lowBarCrossNum.requestFocusFromTouch()
+        }
+
+        lowBarCrossNumPlus.setOnClickListener {
+            lowBarCrossNum.setText((lowBarCrossNum.text.toString().toInt() + 1).toString())
+            lowBarCrossNum.requestFocusFromTouch()
+        }
+
+        roughTerrainCrossNumMinus.setOnClickListener {
+            roughTerrainCrossNum.setText((roughTerrainCrossNum.text.toString().toInt() - 1).toString())
+            roughTerrainCrossNum.requestFocusFromTouch()
+        }
+
+        roughTerrainCrossNumPlus.setOnClickListener {
+            roughTerrainCrossNum.setText((roughTerrainCrossNum.text.toString().toInt() + 1).toString())
+            roughTerrainCrossNum.requestFocusFromTouch()
+        }
+
+        rockWallCrossNumMinus.setOnClickListener {
+            rockWallCrossNum.setText((rockWallCrossNum.text.toString().toInt() - 1).toString())
+            rockWallCrossNum.requestFocusFromTouch()
+        }
+
+        rockWallCrossNumPlus.setOnClickListener {
+            rockWallCrossNum.setText((rockWallCrossNum.text.toString().toInt() + 1).toString())
+            rockWallCrossNum.requestFocusFromTouch()
+        }
+
+        sallyportCrossNumMinus.setOnClickListener {
+            sallyportCrossNum.setText((sallyportCrossNum.text.toString().toInt() - 1).toString())
+            sallyportCrossNum.requestFocusFromTouch()
+        }
+
+        sallyportCrossNumPlus.setOnClickListener {
+            sallyportCrossNum.setText((sallyportCrossNum.text.toString().toInt() + 1).toString())
+            sallyportCrossNum.requestFocusFromTouch()
+        }
+
+        drawbridgeCrossNumMinus.setOnClickListener {
+            drawbridgeCrossNum.setText((drawbridgeCrossNum.text.toString().toInt() - 1).toString())
+            drawbridgeCrossNum.requestFocusFromTouch()
+        }
+
+        drawbridgeCrossNumPlus.setOnClickListener {
+            drawbridgeCrossNum.setText((drawbridgeCrossNum.text.toString().toInt() + 1).toString())
+            drawbridgeCrossNum.requestFocusFromTouch()
+        }
+
+        rampartsCrossNumMinus.setOnClickListener {
+            rampartsCrossNum.setText((rampartsCrossNum.text.toString().toInt() - 1).toString())
+            rampartsCrossNum.requestFocusFromTouch()
+        }
+
+        rampartsCrossNumPlus.setOnClickListener {
+            rampartsCrossNum.setText((rampartsCrossNum.text.toString().toInt() + 1).toString())
+            rampartsCrossNum.requestFocusFromTouch()
+        }
+
+        moatCrossNumMinus.setOnClickListener {
+            moatCrossNum.setText((moatCrossNum.text.toString().toInt() - 1).toString())
+            moatCrossNum.requestFocusFromTouch()
+        }
+
+        moatCrossNumPlus.setOnClickListener {
+            moatCrossNum.setText((moatCrossNum.text.toString().toInt() + 1).toString())
+            moatCrossNum.requestFocusFromTouch()
+        }
+
+        portcullisCrossNumMinus.setOnClickListener {
+            portcullisCrossNum.setText((portcullisCrossNum.text.toString().toInt() - 1).toString())
+            portcullisCrossNum.requestFocusFromTouch()
+        }
+
+        portcullisCrossNumPlus.setOnClickListener {
+            portcullisCrossNum.setText((portcullisCrossNum.text.toString().toInt() + 1).toString())
+            portcullisCrossNum.requestFocusFromTouch()
+        }
+
+        chevalDeFrisseCrossNumMinus.setOnClickListener {
+            chevalDeFrisseCrossNum.setText((chevalDeFrisseCrossNum.text.toString().toInt() - 1).toString())
+            chevalDeFrisseCrossNum.requestFocusFromTouch()
+        }
+
+        chevalDeFrisseCrossNumPlus.setOnClickListener {
+            chevalDeFrisseCrossNum.setText((chevalDeFrisseCrossNum.text.toString().toInt() + 1).toString())
+            chevalDeFrisseCrossNum.requestFocusFromTouch()
+        }
+
+        lowGoalMissNumMinus.setOnClickListener {
+            lowGoalMissNum.setText((lowGoalMissNum.text.toString().toInt() - 1).toString())
+            lowGoalMissNum.requestFocusFromTouch()
+        }
+
+        lowGoalMissNumPlus.setOnClickListener {
+            lowGoalMissNum.setText((lowGoalMissNum.text.toString().toInt() + 1).toString())
+            lowGoalMissNum.requestFocusFromTouch()
+        }
+
+        lowGoalNumMinus.setOnClickListener {
+            lowGoalNum.setText((lowGoalNum.text.toString().toInt() - 1).toString())
+            lowGoalNum.requestFocusFromTouch()
+        }
+
+        lowGoalNumPlus.setOnClickListener {
+            lowGoalNum.setText((lowGoalNum.text.toString().toInt() + 1).toString())
+            lowGoalNum.requestFocusFromTouch()
+        }
+
+        highGoalMissNumMinus.setOnClickListener {
+            highGoalMissNum.setText((highGoalMissNum.text.toString().toInt() - 1).toString())
+            highGoalMissNum.requestFocusFromTouch()
+        }
+
+        highGoalMissNumPlus.setOnClickListener {
+            highGoalMissNum.setText((highGoalMissNum.text.toString().toInt() + 1).toString())
+            highGoalMissNum.requestFocusFromTouch()
+        }
+
+        highGoalNumMinus.setOnClickListener {
+            highGoalNum.setText((highGoalNum.text.toString().toInt() - 1).toString())
+            highGoalNum.requestFocusFromTouch()
+        }
+
+        highGoalNumPlus.setOnClickListener {
+            highGoalNum.setText((highGoalNum.text.toString().toInt() + 1).toString())
+            highGoalNum.requestFocusFromTouch()
+        }
     }
 }
